ebe438358f7e1fec4108f7a196960a5900fe08fd
Luis Lopez <luis_mty_95@hotmail.com>
2018-06-14 14:23:04 -0500

    Parametros faltantes

--- jenkins/jobs/dsl/doa17_environment_pipeline.groovy
+++ jenkins/jobs/dsl/doa17_environment_pipeline.groovy
@@ -98,6 +98,11 @@ doa17CreateApplication.with{
   }
   parameters{
     stringParam("AWS_REGION",'',"Default AWS Region")
+    stringParam("ENVIRONMENT_NAME",'',"Name of your Environment")
+    stringParam("WEB_APP_PROFILE",'',"Web App Instance Profile from DevOps-Workshop-Networking stack")
+    stringParam("WEB_APP_SG",'',"Web App SG from DevOps-Workshop-Networking stack")
+    stringParam("PUBLIC_SUBNET",'',"Public Subnet from DevOps-Workshop-Networking stack")
+    stringParam("CODE_DEPLOY_ARN",'',"IAM Role ARN from DevopsWorkshop-raem-roles stack")
 
   }
   wrappers {
@@ -140,6 +145,11 @@ doa17CreateDevelopmentGroup.with{
   }
   parameters{
     stringParam("AWS_REGION",'',"Default AWS Region")
+    stringParam("ENVIRONMENT_NAME",'',"Name of your Environment")
+    stringParam("WEB_APP_PROFILE",'',"Web App Instance Profile from DevOps-Workshop-Networking stack")
+    stringParam("WEB_APP_SG",'',"Web App SG from DevOps-Workshop-Networking stack")
+    stringParam("PUBLIC_SUBNET",'',"Public Subnet from DevOps-Workshop-Networking stack")
+    stringParam("CODE_DEPLOY_ARN",'',"IAM Role ARN from DevopsWorkshop-raem-roles stack")
   }
   wrappers {
     preBuildCleanup()
@@ -180,6 +190,11 @@ doa17CreateProductionGroup.with{
   }
   parameters{
     stringParam("AWS_REGION",'',"Default AWS Region")
+    stringParam("ENVIRONMENT_NAME",'',"Name of your Environment")
+    stringParam("WEB_APP_PROFILE",'',"Web App Instance Profile from DevOps-Workshop-Networking stack")
+    stringParam("WEB_APP_SG",'',"Web App SG from DevOps-Workshop-Networking stack")
+    stringParam("PUBLIC_SUBNET",'',"Public Subnet from DevOps-Workshop-Networking stack")
+    stringParam("CODE_DEPLOY_ARN",'',"IAM Role ARN from DevopsWorkshop-raem-roles stack")
   }
   wrappers {
     preBuildCleanup()
